02f96240bc03dc597882be2c93ac27f29cf43eb3
DanielTGbigdeveloper <dandantutorsgh@gmail.com>
2021-04-17 10:19:14 -0300

    Renaming

--- DnlibObfuscator/Program.cs
+++ DnlibObfuscator/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using dnlib.DotNet.Writer;
 
 namespace DnlibObfuscator
 {
@@ -10,6 +13,30 @@ namespace DnlibObfuscator
     {
         static void Main(string[] args)
         {
+            try
+            {
+                ModuleDef mod = ModuleDefMD.Load(args[0]); //We load the dragged .exe
+
+                Renamer.ExecuteRenamer(mod); //Execute renaming phase.
+
+                SaveExecutable(mod, args[0]); // Save our executable
+                
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("The application is not .net or there was an error in the obfuscation. Try debugging."); //There was an error reading the .exe or in the obfuscation
+                Console.ReadKey();
+            }
+        }
+
+        public static void SaveExecutable(ModuleDef module, string args)
+        {
+            module.Write(args[0] + "-obfuscated.exe", new ModuleWriterOptions(module) //Here we save our obfuscated program
+            {
+                PEHeadersOptions = { NumberOfRvaAndSizes = 13 },
+                Logger = DummyLogger.NoThrowInstance
+            });
         }
     }
 }
---
+++ DnlibObfuscator/Renamer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace DnlibObfuscator
+{
+    class Renamer
+    {
+        public static Random random = new Random();
+
+        public static string Characters = "俺01234ム仮俺56789ム仮";
+        private static string RandomString(int length, string characters)
+        {
+            return new string(Enumerable.Repeat(characters, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static void ExecuteRenamer(ModuleDef module)
+        {
+            foreach (TypeDef type in module.Types)
+            {
+                type.Name = RandomString(15, Characters);
+                type.Namespace = RandomString(15, Characters);
+                if (type.IsGlobalModuleType || type.IsRuntimeSpecialName || type.IsSpecialName || type.IsWindowsRuntime || type.IsInterface)
+                {
+                    continue;
+                }
+                foreach(MethodDef method in type.Methods)
+                {
+                    if (method.IsConstructor || method.IsRuntimeSpecialName || method.IsRuntime || method.IsStaticConstructor || method.IsVirtual) continue;
+                    method.Name = RandomString(random.Next(80, 120), Characters);
+                    foreach (var field in type.Fields)
+                    {
+                        field.Name = RandomString(random.Next(80, 120), Characters);
+                        foreach (EventDef eventdef in type.Events)
+                        {
+                            eventdef.Name = RandomString(random.Next(80, 120), Characters);
+                            foreach (PropertyDef property in type.Properties)
+                            {
+                                if (property.IsRuntimeSpecialName) continue;
+                                property.Name = RandomString(random.Next(80, 120), Characters);
+                            }
+                        }
+                    }
+
+                    foreach(ParamDef p in method.ParamDefs)
+                    {
+                        p.Name = RandomString(15, Characters);
+                    }
+                }
+            }
+        }
+    }
+}
